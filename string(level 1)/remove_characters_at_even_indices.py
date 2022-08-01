5c7da3d2fbed53b0ea3816e91ffc54b4f810804d
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2022-08-02 00:21:53 +0200

    corrections 14.challenge

--- string(level 1)/remove_characters_at_even_indices.py
+++ string(level 1)/remove_characters_at_even_indices.py
@@ -4,20 +4,32 @@
 # "Coding" -> "oig"
 # "Pizza" -> "iz"
 # "Python" -> "yhn"
-# "A" -> "A"
+# "A" -> ""
+# "" -> ""
 
-s = "A"
+s = "Pizzab"
+# -----------
+# Option 1
+# -----------
+
+# def remove_characters(myString):
+#     new_string = ""
+#     for i in range(len(myString)):
+#         if i%2 != 0:
+#             new_string += myString[i]
+
+#     print(new_string)
+
+# -----------
+# Option 2
+# -----------
 
 def remove_characters(myString):
     new_string = ""
-    longeur = len(myString)
-    if longeur == 0 or longeur == 1:
-        print(myString)
-    else:
-        for i in range(longeur):
-            if (i % 2) != 0:
-                new_string += myString[i]
-    
+    for i in range(1, len(myString), 2):
+        new_string += myString[i]
+        
     print(new_string)
 
-remove_characters("A")
+remove_characters("Pizaab")
+
