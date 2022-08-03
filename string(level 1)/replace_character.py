674e3d4c93f3f50d7c0ec1a18d0b6279482e1a80
thanhvo <72513778+thanhvo-uparis@users.noreply.github.com>
2022-08-03 16:53:04 +0200

    correction for 20.challenge

--- string(level 1)/replace_character.py
+++ string(level 1)/replace_character.py
@@ -9,16 +9,35 @@ curr_char = "l"
 new_char = "s"
 new_string = ""
 
-# Dans le cas que curr_char n'existe pas dans string s
-if curr_char not in s:
-    print(s)
+# --------------
+# Solution 1
+# --------------
+# for char in s:
+#     if char == curr_char:
+#         new_string += new_char
+#     else:
+#         new_string += char
 
-# sinon
-else:
-    for i in s:
-        if i == curr_char:
-            new_string += new_char
-        else:
-            new_string += i
+# print(new_string)
 
+# --------------
+# Solution 2
+# Utilise method replace(curr_char, new_char)
+# --------------
+new_string = s.replace(curr_char, new_char)
 print(new_string)
+
+# --------------
+# Old solution
+# --------------
+# Dans le cas que curr_char n'existe pas dans string s
+# if curr_char not in s:
+#     print(s)
+# else:
+#     for i in s:
+#         if i == curr_char:
+#             new_string += new_char
+#         else:
+#             new_string += i
+
+# print(new_string)
